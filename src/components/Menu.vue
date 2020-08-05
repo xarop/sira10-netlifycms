c58b3f6d990c0439f74c41b8475628c57ac01b57
xarop <ajl@xarop.com>
2020-08-05 13:58:42 +0200

    vue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav>
+<div>
+  <nav id="menu">
     <a href="#">Espais de cotreball</a>
     <a href="#">Espais per vendre</a>
     <a href="#">Espais per tallers</a>
@@ -10,6 +11,7 @@
       <svg v-if="menuOpen" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
       <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
   </button>
+</div>
 </template>
 
 <script>
@@ -23,15 +25,15 @@ export default {
     toggleMenu() {
       this.menuOpen = !this.menuOpen
 
-      // This is using a script that is added in index.html
-    //   window.__setPreferredTheme(
-    //     this.menuOpen ? 'close' : 'open'
-    //   )
+      //This is using a script that is added in index.html
+      // window.__setPreferredMenu(
+      //   this.menuOpen ? 'close' : 'open'
+      // )
     
     }
   },
   mounted() {
-    if (window.__theme == 'close') this.menuOpen = true
+    // if (window.__theme == 'close') this.menuOpen = true
   }
 }
 </script>
